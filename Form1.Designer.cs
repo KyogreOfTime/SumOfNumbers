8dd761c8e4c70f5b2f73a0a75c6b12cdd24c4632
KyogreOfTime <75351912+KyogreOfTime@users.noreply.github.com>
2020-12-12 17:48:23 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace SumOfNumbers
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DescriptLabel1 = new System.Windows.Forms.Label();
+            this.DescriptLabel2 = new System.Windows.Forms.Label();
+            this.ResultLabel = new System.Windows.Forms.Label();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // DescriptLabel1
+            // 
+            this.DescriptLabel1.AutoSize = true;
+            this.DescriptLabel1.Location = new System.Drawing.Point(129, 40);
+            this.DescriptLabel1.Name = "DescriptLabel1";
+            this.DescriptLabel1.Size = new System.Drawing.Size(282, 20);
+            this.DescriptLabel1.TabIndex = 0;
+            this.DescriptLabel1.Text = "Enter Numbers (separated with commas):";
+            // 
+            // DescriptLabel2
+            // 
+            this.DescriptLabel2.AutoSize = true;
+            this.DescriptLabel2.Location = new System.Drawing.Point(12, 203);
+            this.DescriptLabel2.Name = "DescriptLabel2";
+            this.DescriptLabel2.Size = new System.Drawing.Size(287, 20);
+            this.DescriptLabel2.TabIndex = 1;
+            this.DescriptLabel2.Text = "Sum of Entered (non-repeating) Numbers:";
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ResultLabel.Location = new System.Drawing.Point(332, 203);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(188, 25);
+            this.ResultLabel.TabIndex = 2;
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(211, 134);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(94, 29);
+            this.SumButton.TabIndex = 3;
+            this.SumButton.Text = "Calculate";
+            this.SumButton.UseVisualStyleBackColor = true;
+            this.SumButton.Click += new System.EventHandler(this.SumButton_Click);
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(198, 83);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(125, 27);
+            this.InputTextBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 246);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.DescriptLabel2);
+            this.Controls.Add(this.DescriptLabel1);
+            this.Name = "Form1";
+            this.Text = "Sum of all given numbers, without repeats";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label DescriptLabel1;
+        private System.Windows.Forms.Label DescriptLabel2;
+        private System.Windows.Forms.Label ResultLabel;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.TextBox InputTextBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SumOfNumbers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SumButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //grabs user's input
+                string userInput = InputTextBox.Text;
+
+                //isolates commas from the string
+                char[] commas = { ',' };
+
+                //creates a string array of the numbers the user typed
+                string[] numberStrArray = userInput.Split(commas);
+
+                //converts the string array into an int array
+                int[] numbers = Array.ConvertAll(numberStrArray, int.Parse);
+
+                //sorts the int array in ascending order
+                //note: I wasn't consistently getting the correct total
+                //unless the numbers were sorted like this
+                Array.Sort(numbers);
+
+                //creates a int variable to hold the total
+                //and temporarily set its value to 0
+                int total = 0;
+
+                //bool list to check if items are true or false
+                List<bool> visited = new List<bool>();
+ 
+                for (int index = 0; index < numbers.Length; index++)
+                {
+                    visited.Add(false);
+                }
+
+                //index is set back to 0 in this for loop
+                for (int index = 0; index < numbers.Length; index++)
+                {
+                    if (visited[index] == true)
+                    {
+                        //passes control to code below
+                        continue;
+                    }
+
+                    //to grab numbers that occurr only once
+                    int count = 1;
+
+                    for (int index2 = index + 1; index2 < numbers.Length; index2++)
+                    {
+                        if (numbers[index] == numbers[index2])
+                        {
+                            count++;
+
+                            //sets indexed element(s) to true
+                            visited.Insert(index2, true);
+                        }
+                    }
+
+                    if (count == 1)
+                    {
+                        total += numbers[index];
+                    }
+
+                }
+
+                //converts total to string for easy display
+                string totalDisplay = total.ToString();
+                ResultLabel.Text = totalDisplay;
+            }
+            catch
+            {
+                //displays error text if user input is not valid
+                ResultLabel.Text = "Invalid input.";
+            }
+        }
+    }
+}
